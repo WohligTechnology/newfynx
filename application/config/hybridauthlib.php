592c300cfa6ef187eb4b8e8a50bb668e501e40ce
Pooja <pooja.wohlig@gmail.com>
2015-12-07 12:12:51 +0530

    changes

--- application/config/hybridauthlib.php
+++ application/config/hybridauthlib.php
@@ -31,14 +31,14 @@ $config =
 
 			"Google" => array (
 				"enabled" => true,
-				"keys"    => array ( "id" => "937720646213-dmaacrh1dedl6fpqs0n9hosb6sf9vd0q.apps.googleusercontent.com", "secret" => "SkriBBK9NdYvE1gF9nYrkWtF" )
+				"keys"    => array ( "id" => "603941428173-cdtk9skg8f2e4cjh88gf4prg4e6h4lb9.apps.googleusercontent.com", "secret" => "UqGRdZJO9Dv2MGLHYPZqE0rF" )
 				
 			),
 
 			"Facebook" => array (
 				"enabled" => true,
-				"keys"    => array ( "id" => "362872267247263", "secret" => "7c33f7681c8227c46d493a99f9748cb7" ),
-                "scope"   => "email, user_about_me, user_birthday, user_hometown, user_website, offline_access, read_stream, publish_stream, read_friendlists,publish_actions"
+				"keys"    => array ( "id" => "194741327531742", "secret" => "fbcb9844464ef62fb08791927faef866" ),
+                "scope"   => "email, user_about_me, user_birthday, user_hometown, user_website,publish_actions"
 			),
 
 			"Twitter" => array (
