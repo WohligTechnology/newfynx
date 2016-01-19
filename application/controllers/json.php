bf7aed70b2bf02342f5c2b4bbc10b26195ef3e33
mahesh maurya <mahesh@wohlig.com>
2016-01-19 13:25:45 +0530

    chanegs

--- application/controllers/json.php
+++ application/controllers/json.php
@@ -1889,7 +1889,7 @@ public function getsinglesize()
 
     public function getallslider()
     {
-        $data['message'] = $this->db->query('SELECT `slider`.`id` as `id`,`productimage`.`image` as `image`,`product`.`id` as `link`,`product`.`price` as `price`,`slider`.`order` as `order`  FROM `slider` LEFT OUTER JOIN `product` on `product`.`id`=`slider`.`product` LEFT OUTER JOIN `productimage` ON `productimage`.`product`=`product`.`id` GROUP BY `slider`.`id`  ORDER BY `slider`.`order`,`productimage`.`order`  LIMIT 0,10')->result();
+        $data['message'] = $this->db->query('SELECT `id`, `name`, `link`, `target`, `status`, `image`, `template`, `class`, `text`, `centeralign` FROM `fynx_homeslide` WHERE 1')->result();
         $this->load->view('json', $data);
     }
     public function getdiscountcoupon()
