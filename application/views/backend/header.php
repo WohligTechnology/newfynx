8b3111d3d788d693bf1f191a635b1255b131b3ba
Pooja <pthakare33@gmail.com>
2016-02-24 13:01:34 +0530

    scroll issue sort

--- application/views/backend/header.php
+++ application/views/backend/header.php
@@ -1,45 +1,54 @@
 <!DOCTYPE html>
 <html>
-<title><?php echo $title;?> - myfynx</title>
+<title>
+	<?php echo $title;?> - myFynx</title>
 
 <head>
-	<link rel="shortcut icon" href="<?php echo base_url('assets').'/';?>img/favicon.png" type="image/png"/>
-    <!--Let browser know website is optimized for mobile-->
-    <link rel="stylesheet" href="<?php echo base_url('assets').'/';?>bower_components/Materialize/dist/css/materialize.min.css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link href="<?php echo base_url('assets').'/';?>css/style.css" rel="stylesheet">
-    <link href="<?php echo base_url('assets').'/';?>css/font-awesome.min.css" rel="stylesheet">
-    <link href="<?php echo base_url('assets').'/';?>css/jquery.fancybox.css" rel="stylesheet">
-    <link rel="stylesheet" href="<?php echo base_url('assets').'/';?>css/linearfonts.css">
-    
-    <script src="<?php echo base_url('assets').'/';?>bower_components/jquery/dist/jquery.min.js"></script>
-    <script src="<?php echo base_url('assets').'/';?>bower_components/Materialize/dist/js/materialize.min.js"></script>
-    <script src="<?php echo base_url('assets').'/';?>js/chintantable.js"></script>
-    <script src="<?php echo base_url('assets').'/';?>js/jquery.fancybox.pack.js"></script>
-    <script src="<?php echo base_url('assets').'/';?>tinymce/tinymce.min.js"></script>
-    <script src="<?php echo base_url('assets').'/';?>js/formInit.js"></script>
+	<link rel="shortcut icon" href="<?php echo base_url('assets').'/';?>img/favicon.png" type="image/png" />
+	<!--Let browser know website is optimized for mobile-->
+
+	<!-- Compiled and minified CSS -->
+	 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css">
+	 <!-- <link rel="stylesheet" href="<?php echo base_url('assets').'/';?>bower_components/Materialize/dist/css/materialize.min.css"> -->
+
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<link href="<?php echo base_url('assets').'/';?>css/style.css" rel="stylesheet">
+	<link href="<?php echo base_url('assets').'/';?>css/font-awesome.min.css" rel="stylesheet">
+	<link href="<?php echo base_url('assets').'/';?>css/jquery.fancybox.css" rel="stylesheet">
+	<link rel="stylesheet" href="<?php echo base_url('assets').'/';?>css/linearfonts.css">
+
+	<script src="<?php echo base_url('assets').'/';?>bower_components/jquery/dist/jquery.min.js"></script>
+
+	<!-- Compiled and minified JavaScript -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
+	<!-- <script src="<?php echo base_url('assets').'/';?>bower_components/Materialize/dist/js/materialize.min.js"></script> -->
 
 
-    <!--Let browser know website is optimized for mobile-->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<script src="<?php echo base_url('assets').'/';?>js/chintantable.js"></script>
+	<script src="<?php echo base_url('assets').'/';?>js/jquery.fancybox.pack.js"></script>
+	<script src="<?php echo base_url('assets').'/';?>tinymce/tinymce.min.js"></script>
+	<script src="<?php echo base_url('assets').'/';?>js/formInit.js"></script>
+
+	<!--Let browser know website is optimized for mobile-->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
 
 <body>
 
 	<header>
-			<nav class="blue darken-4">
+		<nav class="blue darken-4">
 			<?php   $menus = $this->menu_model->viewmenus(); 	  ?>
-			<ul id="slide-out" class="side-nav fixed">
-				<li class="sub-menu logo">
-					
-                    <div class="logo text-center">
-                    <img src="<?php echo base_url('uploads/logo.png'); ?>" width="80" style="    padding-top: 0px;
-    margin-bottom: -21px;">
-                </div>
-                    </li>
-                    <?php  
+				<ul id="slide-out" class="side-nav fixed">
+					<li class="sub-menu logo">
+
+						<div class="logo">
+							<img src="<?php echo base_url('uploads/logo.png'); ?>" width="40" style="margin-top: 15px;
+    margin-left: 15px; margin-right: 5px;"> My Fynx
+						</div>
+					</li>
+					<?php
 				foreach($menus as $row)
-				{  
+				{
 					$pieces = explode("/", $row->url);
 					$page2="";
 					if(empty($pieces) || !isset($pieces[1]))
@@ -50,25 +59,25 @@
 						$page2=$pieces[1];
 					$submenus = $this->menu_model->getsubmenus($row->id);
 					?>
-                        <li class="<?php if($page==$page2 ||  $page == strtolower($row->name)) {  } 
-					if(count($submenus > 0)) 
-					{ 
+						<li class="<?php if($page==$page2 ||  $page == strtolower($row->name)) {  }
+					if(count($submenus > 0))
+					{
 						$pages =  $this->menu_model->getpages($row->id);
-						//echo $page2; 
+						//echo $page2;
 						//print_r($pages);
-						echo ' sub-menu'; 
+						echo ' sub-menu';
 						if(in_array($page, $pages,TRUE))
 							echo ' active';
 					}
 					?> ">
-                            <a class="waves-effect waves-default" href="<?php 
+							<a class="waves-effect waves-default" href="<?php
 						if($row->url == " ")
 							echo "javascript:; ";
 						else if($row->linktype == 1) echo site_url($row->url);
 						else if($row->linktype == 2) echo base_url($row->url);
-						else if($row->linktype == 3) echo ($row->url);						
+						else if($row->linktype == 3) echo ($row->url);
 						?>" <?php if($row->linktype == 3) echo ""; ?>>
-							<?php  
+							<?php
 							if($row->icon != "")
 							{  ?>
 								<i class="<?php echo $row->icon; ?>"></i>
@@ -77,15 +86,15 @@
 							<span><?php echo $row->name;  ?></span>
 							<span class="arrow"></span>
 						</a>
-                            <?php
+							<?php
 						if(count($submenus) > 0)
 						{  ?>
-                                <ul class="sub">
-                                    <?php
+								<ul class="sub">
+									<?php
 								foreach($submenus as $row2)
-								{ 
+								{
 									$pieces2 = explode("/", $row2->url);
-					
+
 									if(empty($pieces2) || !isset($pieces2[1]))
 									{
 										$page3="";
@@ -93,43 +102,43 @@
 									else
 										$page3=$pieces2[1];
 								?>
-                                        <li class="<?php if($page==$page3 || $page == strtolower($row2->name)) { echo 'active'; } ?> nopadding">
-                                            <a class="waves-effect waves-default" href="<?php 
+										<li class="<?php if($page==$page3 || $page == strtolower($row2->name)) { echo 'active'; } ?> nopadding">
+											<a class="waves-effect waves-default" href="<?php
 											if($row2->url == " ")
 												echo "javascript:; ";
 											else if($row2->linktype == 1) echo site_url($row2->url);
 											else if($row2->linktype == 2) echo base_url($row2->url);
-											else if($row2->linktype == 3) echo ($row2->url);		
+											else if($row2->linktype == 3) echo ($row2->url);
 										?>">
-                                                <?php  
+												<?php
 											if($row2->icon != "")
 											{  ?>
-                                                    <i class="<?php echo $row2->icon; ?>" <?php if($row2->linktype == 3) echo ""; ?>></i>
-                                                    <?php	}
+													<i class="<?php echo $row2->icon; ?>" <?php if($row2->linktype == 3) echo ""; ?>></i>
+													<?php	}
 											?>
-                                                        <?php echo $row2->name;  ?>
-                                            </a>
-                                        </li>
-                                        <?php	}
+														<?php echo $row2->name;  ?>
+											</a>
+										</li>
+										<?php	}
 								?>
-                                </ul>
-                                <?php	}
+								</ul>
+								<?php	}
 						?>
-                        </li>
-                        <?php }
+						</li>
+						<?php }
 				?>
-                </ul>
+				</ul>
 
-                <div class="row">
-                    <div class="col s6">
-                        <a href="#" data-activates="slide-out" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
-                    </div>
-                    <div class="col s6 offset-l6 m6 l6 search">
-                        <a href="<?php echo site_url('login/logout'); ?>" class="waves-effect waves-light btn red" style="float:right; margin: 7px 0 0;"><i class="material-icons left">power_settings_new</i> Logout</a>
-                    </div>
-                </div>
-        </nav>
+				<div class="row">
+					<div class="col s6">
+						<a href="#" data-activates="slide-out" class="button-collapse"><i class="mdi-navigation-menu"></i></a>
+					</div>
+					<div class="col s6 offset-l6 m6 l6 search">
+						<a href="<?php echo site_url('login/logout'); ?>" class="waves-effect waves-light btn red" style="float:right; margin: 7px 0 0;"><i class="material-icons left" style="line-height: inherit;height:auto;">power_settings_new</i> Logout</a>
+					</div>
+				</div>
+		</nav>
 
 
-    </header>
-    <main>
+	</header>
+	<main>
