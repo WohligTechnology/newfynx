eee1b6e8d39ba4be09beb30812f3f9de99e265f3
Pooja <pthakare33@gmail.com>
2016-03-03 18:38:28 +0530

    inventory le le

--- application/models/restapi_model.php
+++ application/models/restapi_model.php
@@ -210,7 +210,7 @@ class restapi_model extends CI_Model
                         if($updateproductqty < 10){
                             $this->load->library('email');
                             $this->email->from('vigwohlig@gmail.com', 'MyFynx');
-                            $this->email->to($email);
+                            $this->email->to("jagruti@wohlig.com");
                             $this->email->subject('Inventory');   
                             $message = "Product Id : ". $product. "Quantity is ".$updatedquantity;
                             $this->email->message($message);
