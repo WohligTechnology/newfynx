748ebcd760ab0687364235887596963ce40a521f
Pooja <pthakare33@gmail.com>
2016-03-04 17:22:10 +0530

    forgot password done ekdum proper

--- application/controllers/json.php
+++ application/controllers/json.php
@@ -2268,64 +2268,68 @@ public function getsinglesize()
             $this->load->view('json', $data);
         } else {
             $hashvalue = base64_encode($userid.'&access');
-            $link = "<a href='http://selfcareindia.com/#/forgotpassword/$hashvalue'>Click here </a> To Reset Your Password.";
+            $link = "<a href='http://myfynx.com/#/forgotpassword/$hashvalue'>Click here </a> To Reset Your Password.";
 
-            $this->load->library('email');
-            $this->email->from('info@myfynx.com', 'Selfcare');
-            $this->email->to($email);
-            $this->email->subject('MyFynx Password Changed');
+            // email srarts
+             $this->load->library('email');
+        $this->email->from('info@myfynx.com', 'MyFynx');
+        $this->email->to($email);
+        $this->email->subject('Forgot Password');
+        $message = "<html><body style='margin: 0;'>
+          <div class='fynxmailer' style='width: 600px; max-width:600px; margin: 0 auto;'>
+            <header>
+              <img src='http://wohlig.co.in/myfynx/img/emailer-fynx.png' alt='' class='img-responsive'>
+            </header>
+            <main>
+              <div class=''>
+                <div class='section-login' style='margin: 0 20px;'>
+                  <p style='font-family: Roboto;font-size: 20px;color: #000;'>Dear <span style='font-family: Roboto;font-size: 20px;color: #000;'>$username</span>,</p>
+                  <p style='font-family: Roboto;font-size: 20px;color: #000;'>Kindly click on the link below to reset your Myfynx password.</p>";
 
-            $message = "<html><body><div id=':1fn' class='a3s adM' style='overflow: hidden;'><div class='HOEnZb'><div class='adm'><div id='q_152da6db6beee01c_0' class='ajR h4' data-tooltip='Hide expanded content' aria-label='Hide expanded content'><div class='ajT'></div></div></div><div class='im'><u></u>
-    						 <div style='margin:0'>
-
-    						 <u></u>
-    						 <div style='margin:0 auto;width:90%'>
-    						 <div style='margin:50px auto;width:80%'>
-    						 <div style='text-align:center' align='center'>
-    						  <img src='http://selfcareindia.com/img/logo.png' alt='Selfcare' class='CToWUd'>
-    						 </div>
-    						 <p style='color:#000;font-family:Roboto;font-size:20px'>Dear <span style='color:#000;font-family:Roboto;font-size:20px'>$username</span>,</p>
-    						 <p style='color:#000;font-family:Roboto;font-size:20px'>Kindly click on the link below to reset your SelfCare password.
-</p>
-    						 <p style='color:#000;font-family:Roboto;font-size:20px'>$link</p>
-
-    						 <span style='color:#000;font-family:Roboto;font-size:20px'>Thank You,</span>
-    						 <span style='color:#000;display:block;font-family:Roboto;font-size:20px'>Team Selfcare !</span>
-    						 </div>
-    						 </div>
-    						 <u></u>
-    						 <footer style='background:#e96542;padding:10px 0'>
-    						 <div style='margin:0 auto;width:90%'>
-    						 <div>
-    						 <table>
-    						 <tbody><tr>
-    						 <td style='padding:0 15px'><div>
-    						 <span style='color:#ffd8ce;font-family:Roboto;font-size:14px'>COPYRIGHT@SELFCARE2016</span>
-    						 </div></td>
-    						 <td style='padding:0 15px'><div>
-    						  <span style='color:#ffd8ce;font-family:Roboto;font-size:14px'>CONTACT US<a href='tel:+912261312222' style='color:#ffd8ce;font-family:Roboto;font-size:14px;margin:0px 10px;text-decoration:none' target='_blank'>+91 22 6131 2222</a></span>
-    						 </div></td>
-    						 <td style='padding:0 15px;vertical-align:middle' valign='middle'>
-    						  <div>
-    						  <span style='color:#ffd8ce;display:block;font-family:Roboto;font-size:14px'>FOLLOW US ON</span>
-    						  <a href='https://www.facebook.com/selfcarebysuman' style='color:#ffd8ce;display:inline-block;font-family:Roboto;font-size:18px;margin:3px 5px 0 0' target='_blank'><img src='http://selfcareindia.com/img/selfcare-facebook.png' alt='Facebook' width='20' class='CToWUd'></a>
-    						  <a href='https://twitter.com/selfcarebysuman' style='color:#ffd8ce;display:inline-block;font-family:Roboto;font-size:18px;margin:3px 5px 0 0' target='_blank'><img src='http://selfcareindia.com/img/selfcare-twitter.png' alt='Twitter' width='20' class='CToWUd'></a>
-    						  <a href='https://www.instagram.com/selfcarebysuman' style='color:#ffd8ce;display:inline-block;font-family:Roboto;font-size:18px;margin:3px 5px 0 0' target='_blank'><img src='http://selfcareindia.com/img/selfcare-insta.png' alt='Instagram' width='20' class='CToWUd'></a>
-    						  <a href='https://www.youtube.com/channel/UCVqKgmC6eaMrgPyXoOcOz2A' style='color:#ffd8ce;display:inline-block;font-family:Roboto;font-size:18px;margin:3px 5px 0 0' target='_blank'><img src='http://selfcareindia.com/img/selfcare-youtube.png' alt='Youtube' width='20' class='CToWUd'></a>
-    						 </div>
-    						 </td>
-    						 </tr>
-    						 </tbody></table>
-    						 </div>
-    						 </div>
-    						 </footer>
-    						 </div>
+   
+        $message.="
 
 
-    						 </div></div></div></body></html>";
-            $this->email->message($message);
-            $this->email->send();
+        <p style='font-family: Roboto;font-size: 20px;color: #000;'>$link</p>
+        <p style='font-family: Roboto;font-size: 20px;color: #000;'>If you have any queries, please email us on info@myfynx.com and we’ll be happy
 
+        to assist you.</p>
+        <p style='color: #fc483f;font-family: Roboto;font-size: 20px;'>Happy Shopping !</p>
+        <span style='font-family: Roboto;font-size: 20px;color: #000;'>Thank You,</span>
+        <span class='block' style='font-family: Roboto;font-size: 20px;color: #000;display: block;'>Team My Fynx!</span>
+        </div>
+        </div>
+        </main>
+        <footer style='background-color: #000; padding: 10px 0; color: #fff; margin-top: 20px;'>
+        <div class='footer-wrapper'>
+        <table style='width: 100%;'>
+        <tr>
+          <td style='padding:15px; float:left;'>
+            <div class='copy'>
+              <span style='font-family: Roboto;font-size: 14px;color: #fff;'>COPYRIGHT@MYFYNX2016</span>
+            </div>
+          </td>
+          <td style='padding: 0 15px; text-align: right;'>
+            <div class='follow' style='text-align: center; float: right;'>
+              <span class='block' style='font-family: Roboto;font-size: 14px;color: #fff;display: block;'>FOLLOW US ON</span>
+              <a href='https://www.facebook.com/MyFynx-401315743385366/?fref=ts' target='_blank' class='inline-block' style='font-family: Roboto;font-size: 18px;color: #fff;display: inline-block;margin: 3px 5px 0 0;'><img src='http://wohlig.co.in/myfynx/img/fynx-fb.png' alt='Facebook' width='20'></a>
+              <a href='https://twitter.com/MyFynx' target='_blank' class='inline-block' style='font-family: Roboto;font-size: 18px;color: #fff;display: inline-block;margin: 3px 5px 0 0;'><img src='http://wohlig.co.in/myfynx/img/fynx-twi.png' alt='Twitter' width='20'></a>
+              <a href='https://www.instagram.com/myfynx/' target='_blank' class='inline-block' style='font-family: Roboto;font-size: 18px;color: #fff;display: inline-block;margin: 3px 5px 0 0;'><img src='http://wohlig.co.in/myfynx/img/fynx-insta.png' alt='Instagram' width='20'></a>
+              <a href='https://www.youtube.com/channel/UCIo8qm3zCU8JmDZ1UaHhf3Q' target='_blank' class='inline-block' style='font-family: Roboto;font-size: 18px;color: #fff;display: inline-block;margin: 3px 5px 0 0;'><img src='http://wohlig.co.in/myfynx/img/fynx-youtube.png' alt='Youtube' width='20'></a>
+            </div>
+          </td>
+        </tr>
+        </table>
+        </div>
+        </footer>
+        </div>
+        </body></html>";
+
+        $this->email->message($message);
+        $this->email->send();
+            
+            // email ends
+            
             $data['message'] = new stdClass();
             $data['message']->value = true;
             $this->load->view('json', $data);
