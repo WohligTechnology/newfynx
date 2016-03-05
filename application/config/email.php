4e8b2a048322909cf18cd07ebbe5ca9ce91da4bd
Pooja <pthakare33@gmail.com>
2016-03-05 18:39:56 +0530

    wersdfgh

--- application/config/email.php
+++ application/config/email.php
@@ -10,3 +10,4 @@ $config['smtp_host'] = 'smtp.mandrillapp.com';
 $config['smtp_port'] = 587;
 $config['smtp_user'] = 'info@myfynx.com';
 $config['smtp_pass'] = 'RFvI2aykFD8b74slgl8fuQ';
+$config['newline'] = "\r\n";
