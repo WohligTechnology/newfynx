80e923e9c20ae9476bfbbdb840e13dff4743ef3c
Pooja <pthakare33@gmail.com>
2016-03-26 11:10:46 +0530

    payment failure

--- application/views/emailers/paymentfailure.php
+++ application/views/emailers/paymentfailure.php
@@ -17,8 +17,10 @@
       <div class="">
         <div class="section-login" style="margin: 0 20px;">
           <p style="font-family: Roboto;font-size: 20px;color: #000;">Dear <span style="font-family: Roboto;font-size: 20px;color: #000;"><?php echo $username;?></span>,</p>
-          <p style="font-family: Roboto;font-size: 20px;color: #000;">Payment for Transaction Id <?php echo $transactionid?> is not Successful!</p>
+          <p style="font-family: Roboto;font-size: 20px;color: #000;">Due to a transaction failure, we’re unable to process your order at present. Please try again later so we can ship your order to you soon.!</p>
+          <p style="font-family: Roboto;font-size: 20px;color: #000;">In case you still face problems, do send us a mail at info@myfynx.com or call us at +91 22 6620 5555 and our team will be there to help you out.</p>
           <!-- show if table is needed -->
+<!--
            <table style='width: 100%;'>
                      <thead style=' background-color: #fc483f; color: #fff;'>
                        <th style='padding: 10px; text-transform:uppercase; font-size: 14px'>Item</th>
@@ -39,11 +41,6 @@
          $subtotal=$cart->finalprice;
 
        ?>
-
-
-
-
-
             <tr>
                <td style='text-align:center;'>
                 <figure>
@@ -65,9 +62,11 @@
 ?>
 </tbody>
 </table>
+-->
           <!-- end of the cart table -->
 
-          <p style="font-family: Roboto;font-size: 20px;color: #000;"><a href="http://www.myfynx.com" target="_blank" style="font-size: 20px;color: #fc483f;font-family: Roboto;">Click Here</a> to return to the website for continue shopping.</p>
+<!--          <p style="font-family: Roboto;font-size: 20px;color: #000;"><a href="http://www.myfynx.com" target="_blank" style="font-size: 20px;color: #fc483f;font-family: Roboto;">Click Here</a> to return to the website for continue shopping.</p>-->
+         <p style='color: #fc483f;font-family: Roboto;font-size: 20px;'>Happy Shopping !</p>
           <span style="font-family: Roboto;font-size: 20px;color: #000;">Thank You,</span>
           <span class="block" style="font-family: Roboto;font-size: 20px;color: #000;display: block;">Team My Fynx!</span>
         </div>
